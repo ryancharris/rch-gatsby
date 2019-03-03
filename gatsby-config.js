095dc61fedef8fb5cac30975f9d74cb759489db8
Ryan Harris <harrisryan1@gmail.com>
2019-03-03 10:05:04 -0500

    Update GA plugin config

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,18 +63,6 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-135453133-1",
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
       },
     },
   ],
